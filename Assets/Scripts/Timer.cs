dbea3c3267ba7f7d42d1dac4975669d792723215
anshultomar250802 <tomaranshul88@gmail.com>
2024-08-01 11:53:06 +0530

    Player Movement added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timerText;
+    private float timeRemaining = 120f;
+    private bool timerIsRunning = false;
+
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        //timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
